61a853e461395bd551c034297c220ebc5490b481
houyishuai <houyishuai@wallan-tech.com>
2018-04-18 12:21:43 +0800

    new router

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/views/Home'
-<<<<<<< HEAD
-import Hot from '@/views/Hot'
-import Society from '@/views/Society'
-import Car from '@/views/Car'
-import Focus from '@/views/Focus'
-import Shanghai from '@/views/Shanghai'
-import Military from '@/views/Military'
-import Sports from '@/views/Sports'
-import Finance from '@/views/Finance'
-import Detail from '@/views/Detail'
-=======
+
 import Detail from '@/views/Detail'
 //import Hot from '@/views/Hot'
 //import Society from '@/views/Society'
@@ -22,7 +12,7 @@ import Detail from '@/views/Detail'
 //import Military from '@/views/Military'
 //import Sports from '@/views/Sports'
 //import Finance from '@/views/Finance'
->>>>>>> dev
+
 
 Vue.use(Router)
 
@@ -34,63 +24,12 @@ export default new Router({
       component: Home
     },
     {
-<<<<<<< HEAD
-      path: '/hot',
-      name: 'Hot',
-      component: Hot
-    },
-    {
-      path: '/society',
-      name: 'Society',
-      component: Society
-    },
-    {
-      path: '/car',
-      name: 'Car',
-      component: Car
-    },
-    {
-      path: '/focus',
-      name: 'Focus',
-      component: Focus
-    },
-    {
-      path: '/shanghai',
-      name: 'Shanghai',
-      component: Shanghai
-    },
-    {
-      path: '/military',
-      name: 'Military',
-      component: Military
-    },
-    {
-      path: '/sports',
-      name: 'Sports',
-      component: Sports
-    },
-    {
-      path: '/finance',
-      name: 'Finance',
-      component: Finance
-    },
-    {
-=======
->>>>>>> dev
+
       path: '/newsdetail/:id',
       name: 'Detail',
       component: Detail
     },
-<<<<<<< HEAD
-    {
-        path: '*',
-        redirect: '/home/all?type=_all_'
-    },
-    {
-        path: '',
-        redirect: '/home/all?type=_all_'
-    },
-=======
+
 //  {
 //    path: '/hot',
 //    name: 'Hot',
@@ -139,6 +78,6 @@ export default new Router({
 //      path: '',
 //      redirect: '/home/all?type=__all__'
 //  },
->>>>>>> dev
+
   ]
 })
